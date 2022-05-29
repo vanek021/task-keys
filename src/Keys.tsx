8e104d971b68bf0437300422bef427497af8b871
vanek021 <vanekmenyakin@gmail.com>
2022-05-29 18:03:56 +0500

    completed task

--- src/Keys.tsx
+++ src/Keys.tsx
@@ -1,5 +1,17 @@
 import { IItem } from './index';
+import { Item } from './item';
 
 export function Keys(props: { initialData: IItem[]; sorting: 'ASC' | 'DESC' }) {
-    return <div></div>;
+    if (props.sorting === 'ASC') props.initialData.sort((a, b) => a.id - b.id);
+    else props.initialData.sort((a, b) => b.id - a.id);
+
+    return (
+        <div>
+            {props.initialData.map((item) => (
+                <div key={item.id}>
+                    <Item item={item} />
+                </div>
+            ))}
+        </div>
+    );
 }
